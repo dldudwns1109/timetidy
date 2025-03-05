db81812bd54692f41b29a7c371f150ca00ec0f7b
dldudwns1109 <dldudwns1109@gmail.com>
2025-03-05 12:25:12 +0900

    회원 정보 DB에 저장
    로그인한 후 회원 정보 유무에 따라 DB에 저장하는 기능을 구현하였고 Service를 만들어서 여러 Dao 함수들을 묶어서
    비즈니스 로직을 처리하였습니다. 🎇

--- semi/src/main/java/com/kh/semi/interceptor/MemberLoginInterceptor.java
+++ semi/src/main/java/com/kh/semi/interceptor/MemberLoginInterceptor.java
@@ -1,15 +1,10 @@
 package com.kh.semi.interceptor;
 
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 @Component
 public class MemberLoginInterceptor implements HandlerInterceptor {
@@ -17,28 +12,12 @@ public class MemberLoginInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-        Object authentication = SecurityContextHolder.getContext().getAuthentication();
+		String email = (String) request.getSession().getAttribute("email");
+		if (email == null) {
+			response.sendRedirect("/login");
+			return false;
+		}		
 
-        if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
-            response.sendRedirect("/login");
-            return false;
-        }
-
-        if (authentication instanceof OAuth2AuthenticationToken) {
-            OAuth2AuthenticationToken authenticationToken = (OAuth2AuthenticationToken) authentication;
-            OAuth2User oAuth2User = (OAuth2User) authenticationToken.getPrincipal();
-
-            if (oAuth2User == null) {
-                response.sendRedirect("/login");
-                return false;
-            }
-
-            HttpSession session = request.getSession();
-            session.setAttribute("email", oAuth2User.getAttribute("email"));
-            return true;
-        }
-
-        response.sendRedirect("/login");
-        return false;
+        return true;
 	}
 }
