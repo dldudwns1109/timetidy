265a60b5b601171114da334b11db2036040cb962
dldudwns1109 <dldudwns1109@gmail.com>
2025-03-11 17:27:03 +0900

    소셜 목록 조회 및 삭제 기능 구현
    소셜 목록 조회 및 삭제 기능을 구현하였습니다. ✨

--- semi/src/main/java/com/kh/semi/mapper/SocialMapper.java
+++ semi/src/main/java/com/kh/semi/mapper/SocialMapper.java
@@ -4,9 +4,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 
 import com.kh.semi.dto.SocialDto;
 
+@Component
 public class SocialMapper implements RowMapper<SocialDto> {
 
 	@Override
--- semi/src/main/java/com/kh/semi/restcontroller/SocialRestController.java
+++ semi/src/main/java/com/kh/semi/restcontroller/SocialRestController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.semi.dao.SocialDao;
 import com.kh.semi.dto.MemberSocialDto;
+import com.kh.semi.dto.SocialDto;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -19,6 +20,31 @@ public class SocialRestController {
 	
 	@Autowired
 	private SocialDao socialDao;
+	
+	@GetMapping("/delete")
+	public int delete(@RequestParam int relativeId, HttpSession session) {
+		SocialDto socialDto = new SocialDto();
+		socialDto.setSocialSelfId((int) session.getAttribute("id"));
+		socialDto.setSocialRelativeId(relativeId);
+		socialDao.deleteRelative(socialDto);
+		socialDto.setSocialSelfId(relativeId);
+		socialDto.setSocialRelativeId((int) session.getAttribute("id"));
+		socialDao.deleteRelative(socialDto);
+		return relativeId;
+	}
+	
+	@GetMapping("/list")
+	public List<SocialDto> list(HttpSession session) {
+		return socialDao.socialList((int) session.getAttribute("id"));
+	}
+	
+	@GetMapping("/search-list")
+	public List<SocialDto> searchList(@RequestParam String keyword, HttpSession session) {
+		SocialDto socialDto = new SocialDto();
+		socialDto.setSocialName(keyword);
+		socialDto.setSocialSelfId((int) session.getAttribute("id"));
+		return socialDao.socialSearchList(socialDto);
+	}
 	
 	@GetMapping("/member-list")
 	public List<MemberSocialDto> memberList(@RequestParam String keyword, 
